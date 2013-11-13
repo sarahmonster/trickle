848c7fb6c77fc5d33b75bd58e9b9158fe47de050
sarah <sarah@triggersandsparks.com>
2013-11-13 13:50:52 -0500

    Set up header on phone sizes.

--- header.php
+++ header.php
@@ -47,10 +47,10 @@
 
 				<div id="inner-header" class="wrap clearfix">
 
-					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<a id="mobile-menu" class="image-replacement">Menu</a>
+					
+					<p id="logo"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
 
-					<?php // if you'd like to use the site description you can un-comment it below ?>
 					<?php // bloginfo('description'); ?>
 
 
