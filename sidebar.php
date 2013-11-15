bf84a7991fb1c473c2377c32278ea804f3e7e8f9
sarah <sarah@triggersandsparks.com>
2013-11-15 20:37:15 +0200

    Remove extra widgets from sidebar and clean up white page background.

--- sidebar.php
+++ sidebar.php
@@ -9,14 +9,7 @@
 					<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
 
 						<?php dynamic_sidebar( 'sidebar1' ); ?>
-
-					<?php else : ?>
-
-						<?php // This content shows up if there are no widgets defined in the backend. ?>
-
-						<div class="alert alert-help">
-							<p><?php _e( 'Please activate some Widgets.', 'bonestheme' );  ?></p>
-						</div>
+						
 
 					<?php endif; ?>
 
