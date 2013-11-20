929b0775726e20a687061b03bc1b4120ba4364e9
sarah <sarah@triggersandsparks.com>
2013-11-20 18:54:48 +0200

    Add promos block to homepage.

--- page.php
+++ page.php
@@ -20,12 +20,39 @@
 
 								<section class="entry-content clearfix" itemprop="articleBody">
 									<?php the_content(); ?>
+									
+									<?php if(is_front_page()): ?>
+								<h2><a href="/promotions">Current Promotions</a></h2>
+									<?php
+									$queryObject = new WP_Query( 'post_type=promotions&posts_per_page=1' );
+									// The Loop!
+									if ($queryObject->have_posts()) {
+
+									    while ($queryObject->have_posts()) {
+									        $queryObject->the_post();
+									        ?>
+									
+									 <div class="fourcol first clearfix">
+									 <?php the_post_thumbnail('full'); ?> 
+									 </div>
+									 
+									 <div class="eightcol last clearfix">
+										 <h3><?php the_title(); ?></h3>
+										 <?php the_content(); ?>
+									</div>
+									    <?php
+									    }
+									    ?>
+									    <?php
+									}
+									?>
+							<?php endif; ?>
 							</section> <?php // end article section ?>
 
-								<footer class="article-footer">
-									<?php the_tags( '<span class="tags">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '' ); ?>
-
-								</footer> <?php // end article footer ?>
+							
+							
+							
+							
 
 							</article> <?php // end article ?>
 
