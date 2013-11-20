60080b26768899bec7542f682ada8ce2dbe0b8a9
sarah <sarah@triggersandsparks.com>
2013-11-20 19:42:38 +0200

    Remove title from homepage.

--- page.php
+++ page.php
@@ -11,13 +11,11 @@
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-
-								<header class="article-header">
-
+								<?php if(!is_front_page()): ?>
 									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
-
-								</header> <?php // end article header ?>
-
+								<?php endif; ?>
+								
+								
 								<section class="entry-content clearfix" itemprop="articleBody">
 									<?php the_content(); ?>
 									
