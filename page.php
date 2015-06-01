e74ca3002630eba8a4fc8c72dbfda91b7a76789e
sarah <sarah@triggersandsparks.com>
2015-06-01 23:03:47 +0100

    Remove promotions from homepage if there aren't any.

--- page.php
+++ page.php
@@ -3,7 +3,7 @@
 			<div id="content">
 
 				<div id="inner-content" class="wrap clearfix">
-				
+
 						<?php get_sidebar(); ?>
 
 						<div id="main" class="sevencol onepush clearfix" role="main">
@@ -14,43 +14,43 @@
 								<?php if(!is_front_page()): ?>
 									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
 								<?php endif; ?>
-								
-								
+
+
 								<section class="entry-content clearfix" itemprop="articleBody">
 									<?php the_content(); ?>
-									
-									<?php if(is_front_page()): ?>
-								<h2><a href="/promotions">Current Promotions</a></h2>
-									<?php
-									$queryObject = new WP_Query( 'post_type=promotions&posts_per_page=1' );
-									// The Loop!
-									if ($queryObject->have_posts()) {
 
-									    while ($queryObject->have_posts()) {
-									        $queryObject->the_post();
-									        ?>
-									
-									 <div class="fourcol first clearfix">
-									 <?php the_post_thumbnail('full'); ?> 
-									 </div>
-									 
-									 <div class="eightcol last clearfix">
-										 <h3><?php the_title(); ?></h3>
-										 <?php the_content(); ?>
-									</div>
-									    <?php
-									    }
-									    ?>
-									    <?php
-									}
-									?>
+									<?php if ( is_front_page() AND 0 !== wp_count_posts( 'promotions' )->publish ): ?>
+										<h2><a href="/promotions">Current Promotions</a></h2>
+										<?php
+										$queryObject = new WP_Query( 'post_type=promotions&posts_per_page=1' );
+										// The Loop!
+										if ($queryObject->have_posts()) {
+
+										    while ($queryObject->have_posts()) {
+										        $queryObject->the_post();
+										        ?>
+
+										 <div class="fourcol first clearfix">
+										 <?php the_post_thumbnail('full'); ?>
+										 </div>
+
+										 <div class="eightcol last clearfix">
+											 <h3><?php the_title(); ?></h3>
+											 <?php the_content(); ?>
+										</div>
+										    <?php
+										    }
+										    ?>
+										    <?php
+										}
+										?>
 							<?php endif; ?>
 							</section> <?php // end article section ?>
 
-							
-							
-							
-							
+
+
+
+
 
 							</article> <?php // end article ?>
 
