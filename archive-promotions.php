0c44beb25df330ea803aac83d3a84b712b0bfaed
sarah <sarah@triggersandsparks.com>
2015-06-01 22:46:54 +0100

    Improve error message when no promotions exist.

--- archive-promotions.php
+++ archive-promotions.php
@@ -3,28 +3,28 @@
 			<div id="content">
 
 				<div id="inner-content" class="wrap clearfix">
-				
+
 						<?php get_sidebar(); ?>
 
 						<div id="main" class="sevencol onepush clearfix" role="main">
-						
-						
+
+
 							<h1 class="archive-title"><?php post_type_archive_title(); ?></h1>
 
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix entry-content' ); ?> role="article">
-									 
+
 									 <div class="fourcol first clearfix">
-									 <?php the_post_thumbnail('full'); ?> 
+									 <?php the_post_thumbnail('full'); ?>
 									 </div>
-									 
+
 									 <div class="eightcol last clearfix">
 										 <h3><?php the_title(); ?></h3>
 										 <?php the_content(); ?>
 									</div>
 
-							
+
 							</article> <?php // end article ?>
 
 							<?php endwhile; ?>
@@ -43,15 +43,9 @@
 							<?php else : ?>
 
 									<article id="post-not-found" class="hentry clearfix">
-										<header class="article-header">
-											<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-										</header>
 										<section class="entry-content">
-											<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+											<p><?php _e( 'Sorry, there are currently no ongoing promotions. Please check back later!', 'bonestheme' ); ?></p>
 										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the custom posty type archive template.', 'bonestheme' ); ?></p>
-										</footer>
 									</article>
 
 							<?php endif; ?>
